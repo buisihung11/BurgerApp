57daf3562459f5907e36ecbc4e7d23d848b22eb7
Hingsubiu016 <buisihung321@gmail.com>
2019-11-07 11:11:51 +0700

    Add some test with jest,enzyme,enzyme-adapter, deploy to firebase

--- src/components/Navigation/NavigationItems/NavigationItems.jsx
+++ src/components/Navigation/NavigationItems/NavigationItems.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import "./NavigationItems.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
 
+
+
+
 const navigationItems = props => {
   return (
     <ul className="NavigationItems">
