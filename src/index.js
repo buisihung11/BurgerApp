b263c4145a79792eeaa68ae2b569248a36f74f4e
Hung Bui <buisihung321@gmail.com>
2019-12-26 16:27:39 +0700

    Fix Redux devTool

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import authReducer from "./store/reducers/auth";
 import "./index.css";
 import App from "./App";
 
-const composeEnhancers = process.env.NODE_ENV === 'development' 
-                      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
+
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
